f7d052ef0b7cdd9524b9600fa0a9a592aacbef7c
lixue <582434039@qq.com>
2020-04-22 18:10:09 +0800

    禅道提交

--- element_infos/login_page.py
+++ element_infos/login_page.py
@@ -1,47 +1,43 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from common.log_utils import logger
+from common.base_page import BasePage
 
-current_path = os.path.dirname(__file__)
-driver_path = os.path.join(current_path,'../webdriver/chromedriver.exe')
+class LoginPage(BasePage):
+    def __init__(self,driver):
+        super().__init__(driver)
+        self.username_inputbox = {'element_name':'用户名输入框',
+                                  'locator_type':'xpath',
+                                  'locator_value':'//input[@name="account"]',
+                                  'timeout': 5 }
+        self.password_inputbox = {'element_name': '密码输入框',
+                                  'locator_type': 'xpath',
+                                  'locator_value': '//input[@name="password"]',
+                                  'timeout': 4}
+        self.login_button = {'element_name': '登录按钮',
+                                  'locator_type': 'xpath',
+                                  'locator_value': '//button[@id="submit"]',
+                                  'timeout': 2}
 
-#页面属性
-class LoginPage(object):
-    def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=driver_path)
-        self.driver.implicitly_wait(10)
-        self.driver.maximize_window()
-        self.driver.get('http://106.53.50.202:8999/zentao4/www/my/')
-        #页面属性，用户名
-        self.username_inputbox = self.driver.find_element(By.XPATH,'//input[@name="account"]')
-        #页面属性，密码
-        self.password_inputbox = self.driver.find_element(By.XPATH,'//input[@name="password"]')
-        #登录按钮
-        self.login_button = self.driver.find_element(By.XPATH,'//button[@id="submit"]')
-        #保持登录复选框
-        self.keeplogin_checkbox = self.driver.find_element(By.XPATH,'//input[@id="keepLoginon"]')
 
-#方法，控件的操作
-    def input_username(self,username):
-        self.username_inputbox.send_keys(username)
+    def input_username(self,username): #方法 == 》控件的操作
+        self.input( self.username_inputbox , username )
 
     def input_password(self,password):
-        self.password_inputbox.send_keys(password)
+        self.input( self.password_inputbox , password )
 
     def click_login(self):
-        self.login_button.click()
-
+        self.click( self.login_button )
 
 if __name__=="__main__":
-    login_page = LoginPage()
+    current_path = os.path.dirname(__file__)
+    driver_path = os.path.join(current_path, '../webdriver/chromedriver')
+    driver = webdriver.Chrome(executable_path=driver_path)
+    login_page =  LoginPage(driver)
+    login_page.open_url('http://106.53.50.202:8999/zentao4/www/my/')
     login_page.input_username('lixue')
     login_page.input_password('lixue@123')
     login_page.click_login()
-
-
-
-
-
-
 
 
